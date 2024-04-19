513c5974e680c9f486b44c76e7f8c3446c89dab9
Michael Carr <mcarr2468@gmail.com>
2024-04-18 20:20:05 -0500

    add basic style for navbar

--- header.php
+++ header.php
@@ -14,56 +14,81 @@ https://icons.getbootstrap.com/
         <?php wp_head(); ?>
     </head>
     <body>
-    
 
-<header>
-  <nav class="d-flex justify-content-between align-items-center p-4">
-    <!-- left part -->
-    <div class="d-flex">
-      <a href="/">Logo here</a>
-      <div>
-        <a href="/products">Products</a>
-        <a href="/about">About</a>
-      </div>
-    </div>
+  
 
-    <!-- Middle part -->
-    <div>
-      <input placeholder="Search..."/>
-      <button>
-        <i class="bi bi-search"></i>
-      </button>
-    </div>
+<header class="navbar-container">
+  <nav class="navbar">
 
-    <!-- Right side -->
-    <div class="d-flex align-items-center">
-      <div>
-        <!-- have dropdown menu here -->
-        <button>
+    <a href="/" class="logo-link-container">
+      <h1>Yellow<span class=""> Page</span></h1>
+    </a>
+
+    <section class="navbar-search-container">
+      <div class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          All Pages
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Products</a></li>
+            <li><a class="dropdown-item" href="#">About</a></li>
+          </ul>
+      </div> 
+
+      <form class="navbar-search-form">
+        <input class="navbar-search-input" placeholder="Search..."/>
+        <button class="navbar-search-btn">
+          <i class="bi bi-search"></i>
+        </button>
+      </form>
+    </section>
+
+    <section class="navbar-info-section">
+      <a href="/abc" class="heart-icon-link">
+        <i class="bi bi-heart"></i>
+        <div class="wishlist-amt">0</div>
+      </a>
+
+      <a href="/" class="bag-container">
+        <div class="bag-icon-container">
+          <i class="bi bi-bag"></i>
+          <div class="wishlist-amt">0</div>
+        </div>
+        <span class="bag-price">$0.00</span>
+      </a>
+      <div class="nav-item dropdown">
+        <button class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           <i class="bi bi-person-circle"></i>
         </button>
+        <ul class="dropdown-menu">
+          <!-- When user logged out -->
+          <li><a class="dropdown-item" href="#">Sign Up</a></li>
+          <li><a class="dropdown-item" href="#">Login</a></li>
+          <!-- When user logged in -->
+          <!-- <li><a class="dropdown-item" href="#">Profile</a></li>
+          <li><a class="dropdown-item" href="#">Stats</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Logout</a></li> -->
+        </ul>
       </div>
-
-      <div>
-        <a href="/wishlist">
-          <i class="bi bi-heart"></i>
-        </a>
-      </div>
-
-      <div>
-        <a href="/cart">
-          <i class="bi bi-bag"></i>
-        </a>
-      </div>
-      
-    </div>
+    </section>
   </nav>
-
 </header>
 
-<!-- <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i> -->
 
+
+<!-- <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i> -->
 
 <!-- person: <i class="bi bi-person-circle"></i> -->
 <!-- heart: <i class="bi bi-heart"></i> -->
 <!-- cart: <i class="bi bi-bag"></i> -->
+
+<!-- <i class="bi bi-search"></i> -->
+
+
+<!-- <ul class="dropdown-menu">
+  <li><a class="dropdown-item" href="#">Products</a></li>
+  <li><a class="dropdown-item" href="#">About</a></li>
+  <li><hr class="dropdown-divider"></li>
+  <li><a class="dropdown-item" href="#">Something else here</a></li>
+</ul> -->
