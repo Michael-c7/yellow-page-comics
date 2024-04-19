b5ea8375a9e4b89ce7a984a06c6a83dce4c7536d
Michael Carr <mcarr2468@gmail.com>
2024-04-18 20:19:46 -0500

    add styles for footer

--- footer.php
+++ footer.php
@@ -1,6 +1,26 @@
-<footer>
-Footer here
-</footer>
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top ml-4">
+    <div class="col-md-4 d-flex align-items-center ">
+      <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Yellow Page Comics</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="bi bi-twitter" style="font-size: 24px;"></i>
+        </a>
+        </li>
+        <li class="ms-3">
+            <a class="text-body-secondary" href="#">
+            <i class="bi bi-instagram" style="font-size: 24px;"></i>
+            </a>
+        </li>
+        <li class="ms-3">
+            <a class="text-body-secondary" href="#">
+                <i class="bi bi-facebook" style="font-size: 24px; "></i>
+            </a>
+        </li>
+    </ul>
+  </footer>
 
 <!-- wp_footer is the black bar at the top of the page/ admin bar -->
 <?php wp_footer(); ?>
