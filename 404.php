b59cd39fd07e31665bcfa76e8393a5c70fbfc12c
Michael Carr <mcarr2468@gmail.com>
2024-05-22 21:23:29 -0500

    add 404 page and stuff for index.php

---
+++ 404.php
@@ -0,0 +1,22 @@
+<div style="width: 100%; display: flex; flex-direction:column; justify-content: center; align-items: center; text-align: center;">
+    <div>
+        <?php 
+            echo '<img style="width:500px;" src="wp-content/themes/yellow-page-comics-theme/images//404-page-not-found-9.svg" alt="404 image"/>';
+        ?>
+    </div>
+
+    <div>
+        <h1>The 404 page</h1>
+        <a href="/" class="btn btn-link">Go Home</a>
+
+    </div>
+
+
+
+    <button type="button" class="btn btn-primary position-relative">
+  Profile
+  <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+    <span class="visually-hidden">New alerts</span>
+  </span>
+</button>
+</div>
