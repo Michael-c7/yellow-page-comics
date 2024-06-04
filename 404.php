24192477310c476d04c57c3c43a073d8ca55c155
Michael Carr <mcarr2468@gmail.com>
2024-06-03 22:13:11 -0500

    remove unused code

--- 404.php
+++ 404.php
@@ -8,15 +8,7 @@
     <div>
         <h1>The 404 page</h1>
         <a href="/" class="btn btn-link">Go Home</a>
-
     </div>
 
-
-
-    <button type="button" class="btn btn-primary position-relative">
-  Profile
-  <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
-    <span class="visually-hidden">New alerts</span>
-  </span>
 </button>
 </div>
